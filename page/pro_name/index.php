72748b1d529d6328b3ba476f3fef11a3955e743a
manhldvxut <51306115+manhldvxut@users.noreply.github.com>
2020-11-20 17:52:51 +0900

    test

---
+++ page/pro_name/index.php
@@ -0,0 +1,263 @@
+<?php
+//下記のパス「demo」を変更
+  include_once $_SERVER['DOCUMENT_ROOT'].'/page/pro_name/assets/inc/config.php'; ?>
+<!-- include header --> 
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/'.DIRNAME.'/assets/inc/header.php'; ?>
+<main class="top-main">
+  <section class="top-banner">
+    <div class="bunner wow fadeIn js-bunder" data-wow-delay = "3.5s">
+      <div class="bunner-content">
+        <a href="">
+          <img src="/<?php echo DIRNAME; ?>/assets/images/top/burn.svg" alt="">
+          <div class="d-flex">
+            <small class="wow zoomIn" data-wow-delay = "3.8s">広</small> 
+            <small class="wow zoomIn" data-wow-delay = "4s">告</small>
+            <small class="wow zoomIn" data-wow-delay = "4.2s">出</small>
+            <small class="wow zoomIn" data-wow-delay = "4.4s">稿</small>
+            <small class="wow zoomIn" data-wow-delay = "4.6s">の</small>
+            <small class="wow zoomIn" data-wow-delay = "4.8s">ご</small>
+            <small class="wow zoomIn" data-wow-delay = "5s">案</small>
+            <small class="wow zoomIn" data-wow-delay = "5.2s">内</small>
+          </div>
+        </a>
+        <span class="wow bounceIn" data-wow-delay = "5.5s"><img style="width: auto;" src="/<?php echo DIRNAME; ?>/assets/images/top/arrow-right.svg" alt=""></span>
+      </div>
+    </div>
+    <div class="slo-img">
+      <div class="slogan">
+        <div class="title">
+          <p>ステッカーを貼るだけでガソリン代が無料に！</p>
+                <h2 class="rock"><span>クルマ広告、誕生。</span> </h2>
+            </div> 
+      </div> <!-- end slogan -->
+      <div class="big-img">
+        <div class="banner-top">
+          <img src="/<?php echo DIRNAME; ?>/assets/images/top/banner.jpg" alt="">
+          <ul>
+            <li><img class="wow bounceIn" data-wow-delay = "2.6s" src="/<?php echo DIRNAME; ?>/assets/images/top/pe-01.svg" alt=""></li>
+            <li><img class="wow rubberBand" data-wow-delay="3s" src="/<?php echo DIRNAME; ?>/assets/images/top/car.png" alt=""></li>
+            <li><img class="wow bounceIn" data-wow-delay = "2.6s" src="/<?php echo DIRNAME; ?>/assets/images/top/pe-03.svg" alt=""></li>
+            <li><img class="wow bounceIn" data-wow-delay = "2.6s" src="/<?php echo DIRNAME; ?>/assets/images/top/pe-02.svg" alt=""></li>
+          </ul>
+        </div>
+      </div> <!-- end box img -->
+    </div>
+    <div class="container">
+      <div class="service">
+        <h5 class="two-line">走れば走るほど収入が増える！</h5>
+        <h3>あなたのクルマを広告塔に。</h3>
+        <div class="desc text-center">
+          モーバは、お持ちのクルマにステッカーを貼るだけで、お小遣いが得られるサービス。 <br>1キロ10円の分かりやすい料金体系なので、日々の運転が楽しくなります。 <br>モーバへ登録して、素敵なカーライフを始めてみませんか？
+        </div>
+        <div class="row w-m-3">
+          <div class="col-md-4 w-p-3">
+            <div class="ser-list wow fadeIn">
+              <img class="wow bounceIn" data-wow-delay = "0.2s" src="/<?php echo DIRNAME; ?>/assets/images/top/ser-01.svg" alt="">
+              <div class="ser-inf wow fadeIn" data-wow-delay = "0.5s">
+                <h4>スマホで簡単手続き、<br> いつでもスタート！</h4>
+                <div class="desc">お持ちのクルマに規定サイズのステッカーが貼れれば、どなたでも参加可能！全ての手続きがスマホで完結するので簡単です。</div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 w-p-3">
+            <div class="ser-list wow fadeIn">
+              <img class="wow bounceIn" data-wow-delay = "0.5s" src="/<?php echo DIRNAME; ?>/assets/images/top/ser-02.svg" alt="">
+              <div class="ser-inf wow fadeIn" data-wow-delay = "0.8s">
+                <h4>広告ステッカーはご自身で<br>自由に選んで走行可能！</h4>
+                <div class="desc">一覧から、自分のクルマに貼るステッカーを選ぶことが可能です。３種類のプランから、報酬の上限額を選べます。</div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 w-p-3">
+            <div class="ser-list wow fadeIn">
+              <img class="wow bounceIn" data-wow-delay = "0.8s" src="/<?php echo DIRNAME; ?>/assets/images/top/ser-03.svg" alt="">
+              <div class="ser-inf wow fadeIn" data-wow-delay = "1s">
+                <h4>副業にならないので<br>安心して取り組める！</h4>
+                <div class="desc">副業には当たらないので、企業にお勤めの方も安心して取り組めます。ステッカーを貼ったら、安全運転を心がけましょう！</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> <!-- end service -->
+      <a href="" class="btn-red m-auto"> サービスについて</a>
+    </div> <!-- end container -->
+    <div class="bg-car">
+      <img class="wow rubberBand" data-wow-delay ="1.2s" src="/<?php echo DIRNAME; ?>/assets/images/top/bg-car.svg" alt="">
+    </div>
+  </section>
+  <section class="bg-white recruit-top">
+    <div class="container">
+      <h2 class="line-top">現在募集中の広告</h2>
+      <div class="desc font-weight-bold">対象エリアの広告の中から、お好きなステッカーにエントリーしてください。</div>
+      <small>※ステッカーは随時追加中です。</small>
+      <div class="row w-m-3">
+        <div class="col-md-3 col-6 w-p-3">
+          <div class="recruit-list wow fadeIn" data-wow-delay="0.2s">
+            <a href=""><img src="/<?php echo DIRNAME; ?>/assets/images/top/ads-1.png" alt="">
+              <h3 class="recruit-tit">
+                <img src="/<?php echo DIRNAME; ?>/assets/images/top/yaru.svg">
+                梅田メトロビジョン
+              </h3>
+            </a>
+            
+          </div>
+        </div>
+        <div class="col-md-3 col-6 w-p-3">
+          <div class="recruit-list wow fadeIn" data-wow-delay="0.3s">
+            <a href=""><img src="/<?php echo DIRNAME; ?>/assets/images/top/ads-2.png" alt="">
+              <h3 class="recruit-tit">
+                <img src="/<?php echo DIRNAME; ?>/assets/images/top/yaru.svg">
+                名古屋市医師会
+              </h3>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3 col-6 w-p-3">
+          <div class="recruit-list wow fadeIn" data-wow-delay="0.4s">
+            <a href=""><img src="/<?php echo DIRNAME; ?>/assets/images/top/ads-3.png" alt="">
+              <h3 class="recruit-tit">
+                <img src="/<?php echo DIRNAME; ?>/assets/images/top/yaru.svg">
+                キャリアトレック
+              </h3>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3 col-6 w-p-3">
+          <div class="recruit-list wow fadeIn" data-wow-delay="0.5s">
+            <a href=""><img src="/<?php echo DIRNAME; ?>/assets/images/top/ads-4.png" alt="">
+              <h3 class="recruit-tit">
+                <img src="/<?php echo DIRNAME; ?>/assets/images/top/yaru.svg">
+                企業名
+              </h3>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3 col-6 w-p-3">
+          <div class="recruit-list wow fadeIn" data-wow-delay="0.6s">
+            <a href=""><img src="/<?php echo DIRNAME; ?>/assets/images/top/ads-8.png" alt="">
+              <h3 class="recruit-tit">
+                <img src="/<?php echo DIRNAME; ?>/assets/images/top/yaru.svg">
+                横浜市観光情報
+              </h3>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3 col-6 w-p-3">
+          <div class="recruit-list wow fadeIn" data-wow-delay="0.7s">
+            <a href=""><img src="/<?php echo DIRNAME; ?>/assets/images/top/ads-6.png" alt="">
+              <h3 class="recruit-tit">
+                <img src="/<?php echo DIRNAME; ?>/assets/images/top/yaru.svg">
+                広告タイトルが長い場合はこのように２行で表示します
+              </h3>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3 col-6 w-p-3">
+          <div class="recruit-list wow fadeIn" data-wow-delay="0.8s">
+            <a href=""><img src="/<?php echo DIRNAME; ?>/assets/images/top/ads-6.png" alt="">
+              <h3 class="recruit-tit">
+                <img src="/<?php echo DIRNAME; ?>/assets/images/top/yaru.svg">
+                キャリアトレック
+              </h3>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3 col-6 w-p-3">
+          <div class="recruit-list wow fadeIn" data-wow-delay="0.9s">
+            <a href=""><img src="/<?php echo DIRNAME; ?>/assets/images/top/ads-8.png" alt="">
+              <h3 class="recruit-tit">
+              <img src="/<?php echo DIRNAME; ?>/assets/images/top/yaru.svg">
+                ciatre
+              </h3>
+            </a>
+          </div>
+        </div>
+      </div>
+      <a href="" class="btn-red m-auto btn-search"> ステッカーを探す <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><defs><style>.a{fill:#dc000c;}</style></defs><path class="a" d="M13.148,11.019a6.328,6.328,0,1,0-2.13,2.128l4.293,4.293,2.129-2.13Zm-5.395.64a3.9,3.9,0,1,1,3.905-3.9,3.91,3.91,0,0,1-3.905,3.9Z" transform="translate(-1.44 -1.44)"/></svg></span></a>
+    </div>
+  </section>
+  <section class="user-infomation">
+    <div class="container">
+      <h2 class="line-top">ご利用までの流れ</h2>
+      <div class="row .w-m-3">
+        <div class="col-md-3 w-p-3">
+          <div class="step wow fadeIn" data-wow-delay = "0.5s">
+            <div class="step-num">
+              <span class="text-num"> step</span>
+              <span class="num">01</span>
+            </div>
+            <div class="step-info">
+              <h4>まずは<br class="sp">会員登録</h4>
+              <div class="desc">車のオーナーさんの利用は完全無料！まずは早めに、会員登録をしておきましょう。</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 w-p-3">
+          <div class="step wow fadeIn" data-wow-delay = "0.7s">
+            <div class="step-num">
+              <span class="text-num"> step</span>
+              <span class="num">02</span>
+            </div>
+            <div class="step-info">
+              <h4>ステッカーに<br class="sp">エントリー</h4>
+              <div class="desc">ステッカーを選んでエントリー！広告主の希望プランなども考慮して選んでください。</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 w-p-3">
+          <div class="step wow fadeIn" data-wow-delay="0.9s">
+            <div class="step-num">
+              <span class="text-num"> step</span>
+              <span class="num">03</span>
+            </div>
+            <div class="step-info">
+              <h4>スマホで<br class="sp">簡単手続き</h4>
+              <div class="desc">運営事務局が広告主と相談し掲載の可否を確認、ステッカーをご自宅まで送付します。</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 w-p-3">
+          <div class="step wow fadeIn" data-wow-delay="1.1s">
+            <div class="step-num">
+              <span class="text-num"> step</span>
+              <span class="num">04</span>
+            </div>
+            <div class="step-info">
+              <h4>クルマにステッカーを<br class="sp">貼って、契約開始</h4>
+              <div class="desc">届いたステッカーを貼ったら事務局がビデオ通話で確認。確認後提携期間のスタート。</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a href="" class="btn-gradien m-auto"> まずは新規会員登録</a>
+    </div>
+  </section>
+  <section class="news">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <h2 class="line-top">お知らせ</h2>
+        </div>
+        <div class="col-md-8">
+          <div class="news-info">
+            <ul class="d-flex">
+              <li class="time wow fadeIn">2020.11.14</li>
+              <li class="news-red wow fadeIn">お知らせ</li>
+            </ul>
+            <a class="wow fadeIn" href="/<?php echo DIRNAME; ?>/assets/news/">システムメンテナンスのお知らせ <span class="pdf"></span></a>
+          </div>
+          <div class="news-info">
+            <ul class="d-flex">
+              <li class="time wow fadeIn">2020.11.14</li>
+              <li class="news-red wow fadeIn">サービス</li>
+            </ul>
+            <a class="wow fadeIn" href="/<?php echo DIRNAME; ?>/assets/news/">関西エリアでのサービスを開始しました。</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="small-car wow fadeInRight" data-wow-duration="2s"><img class="wow rubberBand" data-wow-delay="2s" src="/<?php echo DIRNAME; ?>/assets/images/top/small-car.svg"></div>
+  </section>
+</main>
+<!-- include footer --> 
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/'.DIRNAME.'/assets/inc/footer.php'; ?>
